03c3fcd25dcd813786f196fa953c478882f20d2d
Claire Phan <czphan@uci.edu>
2024-03-07 18:24:35 -0800

    bottom bar functional

--- src/_root/RootLayout.tsx
+++ src/_root/RootLayout.tsx
@@ -1,8 +1,21 @@
-import React from 'react'
+import Bottombar from '@/components/shared/Bottombar'
+import LeftSidebar from '@/components/shared/LeftSidebar'
+import Topbar from '@/components/shared/Topbar'
+import { Outlet } from 'react-router-dom'
 
 const RootLayout = () => {
   return (
-    <div>RootLayout</div>
+    <div className="w-full md:flex">
+      <Topbar />
+      <LeftSidebar />
+
+      <section className="flex flex-1 h-full">
+      {/*flex-1 ensures that it takes up all possible horizontal space of flex box, height set to 100%*/}
+        <Outlet />
+      </section>
+
+      <Bottombar />
+    </div>
   )
 }
 
